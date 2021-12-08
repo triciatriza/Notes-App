a3f56bb9c7bc72cab0696d6a67f918091773bdad
Markypops <45309942+Markypops@users.noreply.github.com>
2021-12-08 13:55:42 +0800

    Checkpoint

---
+++ app/src/main/java/com/example/notesapp/dao/NoteDao.kt
@@ -0,0 +1,18 @@
+package com.example.notesapp.dao
+
+import androidx.room.*
+import com.example.notesapp.entities.Notes
+
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * FROM notes ORDER BY id DESC")
+    suspend fun getAllNotes(): List<Notes>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNotes(note:Notes)
+
+    @Delete
+    fun deleteNote(note:Notes)
+}
