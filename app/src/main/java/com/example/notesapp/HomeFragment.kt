14bd7a23fc8ecacb99d4cc81959c08c7f6fff5f8
Markypops <45309942+Markypops@users.noreply.github.com>
2021-12-08 13:57:19 +0800

    Revert "Checkpoint"
    This reverts commit a3f56bb9c7bc72cab0696d6a67f918091773bdad.

--- app/src/main/java/com/example/notesapp/HomeFragment.kt
+++ app/src/main/java/com/example/notesapp/HomeFragment.kt
@@ -5,11 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_home.*
 
 
-class HomeFragment : BaseFragment() {
-
+class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,22 +31,5 @@ class HomeFragment : BaseFragment() {
                 arguments = Bundle().apply {
                 }
             }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        imgNewNote.setOnClickListener{
-            replaceFragment(CreateNoteFragment.newInstance(),true)
-        }
-
-    }
-
-    fun replaceFragment(fragment: Fragment, istransition:Boolean){
-        val fragmentTransition = requireActivity().supportFragmentManager.beginTransaction()
-
-//        if(istransition){
-//            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right, android.R.anim.slide_in_left)
-//        }
-        fragmentTransition.replace(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
     }
 }
