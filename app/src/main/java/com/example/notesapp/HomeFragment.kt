f7afe3856eb511b49daabdf9981485a5638758b7
Markypops <45309942+Markypops@users.noreply.github.com>
2021-12-08 13:13:24 +0800

    Checkpoint

--- app/src/main/java/com/example/notesapp/HomeFragment.kt
+++ app/src/main/java/com/example/notesapp/HomeFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,5 +33,20 @@ class HomeFragment : Fragment() {
                 arguments = Bundle().apply {
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        imageAddImage.setOnClickListener{}
+        replaceFragment(CreateNoteFragment.newInstance(),true)
+    }
+
+    fun replaceFragment(fragment: Fragment, istransition:Boolean){
+        val fragmentTransition = requireActivity()!!.supportFragmentManager.beginTransaction()
+
+//        if(istransition){
+//            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right, android.R.anim.slide_in_left)
+//        }
+        fragmentTransition.replace(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
     }
 }
